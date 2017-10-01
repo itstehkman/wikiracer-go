712b58ec38697b8330351bd9d16e7f538a4ef2e8
Kareem Moussa <itstehkman@gmail.com>
2017-09-30 22:47:41 -0700

    Able to visit children titles

--- wikiracer.go
+++ wikiracer.go
@@ -4,18 +4,50 @@ import (
   "net/http"
   "encoding/json"
   "fmt"
-  "bytes"
+  //"bytes"
   "io/ioutil"
 )
 
-func main() {
-  values := map[string]string{"action": "query", "format": "json", "prop": "links", "title": "Albert+Einstein"}
-  jsonValues, err := json.Marshal(values)
-  if err != nil {
-    fmt.Println(err)
-    return
+type MediaWikiResponse struct {
+  Query map[string](map[string](map[string]interface{}))
+  Continue map[string]string
+}
+
+func (mwr *MediaWikiResponse) childrenTitles() []string {
+  childrenTitles := make([]string, 1)
+  pages := mwr.Query["pages"]
+  for _,v := range pages {
+    links := v["links"].([]interface{})
+    fmt.Println(links)
+    for _,linkmap := range links {
+      lm := linkmap.(map[string]interface{})
+      childrenTitles = append(childrenTitles, lm["title"].(string))
+    }
   }
-  res, err := http.Post("http://www.wikipedia.org/w/api.php", "application/json", bytes.NewBuffer(jsonValues))
+  return childrenTitles
+}
+
+func visitTitle(title, continueParam, plcontinueParam string) {
+  childrenTitles := make([]string, 1)
+  fmt.Println("curr", title)
+
+  req, _ := http.NewRequest("GET", "http://en.wikipedia.org/w/api.php", nil)
+  q := req.URL.Query()
+  q.Add("action", "query")
+  q.Add("format", "json")
+  q.Add("prop", "links")
+  q.Add("titles", title)
+  if continueParam != "" {
+    q.Add("continue", continueParam)
+  }
+  if plcontinueParam != "" {
+    q.Add("plcontinue", plcontinueParam)
+  }
+  req.URL.RawQuery = q.Encode()
+
+  println(req.URL.String())
+
+  res, err := http.Get(req.URL.String())
   println(res.StatusCode)
   if err != nil {
     println("error")
@@ -25,7 +57,35 @@ func main() {
   defer res.Body.Close()
   body, err := ioutil.ReadAll(res.Body)
   var topLevelMap map[string]interface{}
-
+  var mediaWikiResponse MediaWikiResponse
   err = json.Unmarshal(body, &topLevelMap)
-  println(topLevelMap)
+  fmt.Println(topLevelMap)
+  err = json.Unmarshal(body, &mediaWikiResponse)
+  fmt.Println(mediaWikiResponse)
+  if err != nil {
+    fmt.Println("unmarshall failed: ", err)
+  }
+  //fmt.Println(topLevelMap)
+
+  pages := topLevelMap["query"].(map[string]interface{})["pages"].(map[string]interface{})
+  //fmt.Println(pages)
+  for _,v := range pages {
+    m := v.(map[string]interface{})
+    links := m["links"].([]interface{})
+    //fmt.Println(links)
+    for _,linkmap := range links {
+      lm := linkmap.(map[string]interface{})
+      childrenTitles = append(childrenTitles, lm["title"].(string))
+    }
+  }
+  fmt.Println(childrenTitles)
+  for _, childTitle := range childrenTitles {
+    if childTitle != "" {
+      //visitTitle(childTitle, "", "")
+    }
+  }
+}
+
+func main() {
+  visitTitle("Albert Einstein", "", "")
 }
